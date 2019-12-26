e4b848faeeb83501e49f2a1cc033a7bdf3252226
Kawsar Hussen <kawsarhussen@gmail.com>
2019-12-26 01:49:58 -0500

    React Native App Boiler Plate Created and Header Component implemented

---
+++ NumberGuess/App.js
@@ -0,0 +1,20 @@
+
+import Header from './components/Header.js';
+import React from 'react';
+import { StyleSheet,View } from 'react-native';
+
+
+export default function App() {
+  return (
+    <View style={styles.screen}>
+        <Header title= "Guess a Number" />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  screen:{
+    flex: 1,
+  }
+  
+});
